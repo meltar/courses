73a5174067f0bf5bdebc8d7b02507ab3836c90c6
Melissa Holmes <meltar@gmail.com>
2014-03-15 00:30:19 -0400

    Clean up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Courses::Application.routes.draw do
-  get "holes/index"
-  get "holes/show"
-  get "holes/edit"
-  get "holes/update"
-  get "holes/new"
-  get "holes/create"
-  get "holes/delete"
 	root to: 'courses#index'
 
 	resources :courses
+	resources :holes
 
 end
