9d08318086dd8487cd1928c5a8bf1530405eb54d
Melissa Holmes <meltar@gmail.com>
2014-03-15 00:29:43 -0400

    Generate HolesController

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Courses::Application.routes.draw do
+  get "holes/index"
+  get "holes/show"
+  get "holes/edit"
+  get "holes/update"
+  get "holes/new"
+  get "holes/create"
+  get "holes/delete"
 	root to: 'courses#index'
 
 	resources :courses
